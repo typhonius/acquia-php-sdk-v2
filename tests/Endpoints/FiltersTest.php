0b1a2340a9de79940dfeb077a48ad7c7affb412b
Adam Malone <adam@adammalone.net>
2017-11-19 13:04:47 +1100

    Adds filters tests

---
+++ tests/Endpoints/FiltersTest.php
@@ -0,0 +1,52 @@
+<?php
+
+class FiltersTest extends CloudApiTestCase
+{
+
+    public function testAddFilter()
+    {
+
+      /** @var AcquiaCloudApi\CloudApi\Client $object */
+        $object = $this->getMockCloudApiClient();
+
+      $expectedValue = ['filter' => 'name=@"foobar"'];
+      $object->addQuery('filter', 'name=@"foobar"');
+      $property = $this->getPrivateProperty( get_class($object), 'query' );
+      $this->assertEquals( $property->getValue( $object ), $expectedValue );
+
+      $expectedValue = ['filter' => ['name=@"foobar"', 'type=@"baz"']];
+      $object->addQuery('filter', 'type=@"baz"');
+      $property = $this->getPrivateProperty( get_class($object), 'query' );
+      $this->assertEquals( $property->getValue( $object ), $expectedValue );
+
+      $expectedValue = ['sort' => 'title', 'filter' => ['name=@"foobar"', 'type=@"baz"']];
+      $object->addQuery('sort', 'title');
+      $property = $this->getPrivateProperty( get_class($object), 'query' );
+      $this->assertEquals( $property->getValue( $object ), $expectedValue );
+
+      $timezone = new \DateTimeZone('UTC');
+      $time = new \DateTime(date('c'));
+      $time->setTimezone($timezone);
+      $datetime = $time->format(\DateTime::ATOM);
+
+      $expectedValue = ['from' => $datetime, 'to' => $datetime, 'sort' => 'title', 'filter' => ['name=@"foobar"', 'type=@"baz"']];
+      $object->addQuery('from', $datetime);
+      $object->addQuery('to', $datetime);
+      $property = $this->getPrivateProperty( get_class($object), 'query' );
+      $this->assertEquals( $property->getValue( $object ), $expectedValue );
+
+      $expectedValue = [];
+      $object->clearQuery();
+      $property = $this->getPrivateProperty( get_class($object), 'query' );
+      $this->assertEquals( $property->getValue( $object ), $expectedValue );
+      
+    }
+
+  public function getPrivateProperty( $className, $propertyName ) {
+    $reflector = new ReflectionClass( $className );
+    $property = $reflector->getProperty( $propertyName );
+    $property->setAccessible( true );
+
+    return $property;
+  }
+}
--- tests/Endpoints/TasksTest.php
+++ tests/Endpoints/TasksTest.php
@@ -22,7 +22,7 @@ class TasksTest extends CloudApiTestCase
     {
 
         $response = $this->generateCloudApiResponse('Endpoints/getTasks.json');
-        $insights = new \AcquiaCloudApi\Response\TasksResponse($response);
+        $tasks = new \AcquiaCloudApi\Response\TasksResponse($response);
 
 
         $client = $this->getMockBuilder('\AcquiaCloudApi\CloudApi\Client')
@@ -32,7 +32,7 @@ class TasksTest extends CloudApiTestCase
         $client->expects($this->once())
         ->method('tasks')
         ->with('8ff6c046-ec64-4ce4-bea6-27845ec18600')
-        ->will($this->returnValue($insights));
+        ->will($this->returnValue($tasks));
 
         /** @var AcquiaCloudApi\CloudApi\Client $client */
         $result = $client->tasks('8ff6c046-ec64-4ce4-bea6-27845ec18600');
@@ -47,6 +47,56 @@ class TasksTest extends CloudApiTestCase
                 $this->assertObjectHasAttribute($property, $record);
             }
         }
+    }
+
+    public function testGetFilteredTasks()
+    {
+//      $this->cloudapi->addQuery('from', $start->format(\DateTime::ATOM));
+//      $this->cloudapi->addQuery('filter', "name=@*${match}*");
+//      $tasks = $this->cloudapi->tasks($uuid);
+//      $this->cloudapi->clearQuery();
+//
+//      $mock->expects($this->exactly(2))
+//        ->method('set')
+//        ->withConsecutive(
+//          [$this->equalTo('foo'), $this->greaterThan(0)],
+//          [$this->equalTo('bar'), $this->greaterThan(0)]
+//        );
+
+
+      $response = $this->generateCloudApiResponse('Endpoints/getFilteredTasks.json');
+      $insights = new \AcquiaCloudApi\Response\TasksResponse($response);
+
+
+      $client = $this->getMockBuilder('\AcquiaCloudApi\CloudApi\Client')
+        ->disableOriginalConstructor()
+        ->setMethods(['tasks', 'addQuery', 'clearQuery'])
+        ->getMock();
+      $client->expects($this->once())
+        ->method('addQuery')
+        ->with('filter', 'name=@DatabaseBackupCreated');
+      $client->expects($this->once())
+        ->method('tasks')
+        ->with('8ff6c046-ec64-4ce4-bea6-27845ec18600')
+        ->will($this->returnValue($insights));
+      $client->expects($this->once())
+        ->method('clearQuery');
+
+      /** @var AcquiaCloudApi\CloudApi\Client $client */
+      $client->addQuery('filter', 'name=@DatabaseBackupCreated');
+      $result = $client->tasks('8ff6c046-ec64-4ce4-bea6-27845ec18600');
+      $client->clearQuery();
+
+      $this->assertInstanceOf('\ArrayObject', $result);
+      $this->assertInstanceOf('\AcquiaCloudApi\Response\TasksResponse', $result);
+
+      foreach ($result as $record) {
+        $this->assertInstanceOf('\AcquiaCloudApi\Response\TaskResponse', $record);
+
+        foreach ($this->properties as $property) {
+          $this->assertObjectHasAttribute($property, $record);
+        }
+      }
     }
 
 }

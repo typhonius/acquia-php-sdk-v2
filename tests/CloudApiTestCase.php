ebccd0a8bf948dd85bdfe5f4c3f3fcefe29a742e
Adam Malone <adam@adammalone.net>
2017-11-24 14:07:16 +1100

    Adds drush test

--- tests/CloudApiTestCase.php
+++ tests/CloudApiTestCase.php
@@ -23,6 +23,7 @@ abstract class CloudApiTestCase extends TestCase
         $this->assertInstanceOf(Psr7\Stream::class, $stream);
         return $stream;
     }
+
     /**
      * Returns a PSR7 Response (JSON) for a given fixture.
      *
@@ -37,6 +38,21 @@ abstract class CloudApiTestCase extends TestCase
         $this->assertEquals(JSON_ERROR_NONE, json_last_error());
 
         return new Psr7\Response($statusCode, ['Content-Type' => 'application/json'], $stream);
+    }
+
+  /**
+   * Returns a PSR7 Response (Gzip) for a given fixture.
+   *
+   * @param  string        $fixture    The fixture to create the response for.
+   * @param  integer       $statusCode A HTTP Status Code for the response.
+   * @return Psr7\Response
+   */
+    protected function getPsr7GzipResponseForFixture($fixture, $statusCode = 200): Psr7\Response
+    {
+        $stream = $this->getPsr7StreamForFixture($fixture);
+        $this->assertEquals(JSON_ERROR_NONE, json_last_error());
+
+        return new Psr7\Response($statusCode, ['Content-Type' => 'application/gzip'], $stream);
     }
 
     /**
---
+++ tests/Endpoints/DrushTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use AcquiaCloudApi\CloudApi\Client;
+
+class DrushTest extends CloudApiTestCase
+{
+
+    public function testGetApplications()
+    {
+        $response = $this->getPsr7GzipResponseForFixture('Endpoints/getDrushAliases.json');
+        $client = $this->getMockClient($response);
+
+        /** @var AcquiaCloudApi\CloudApi\Client $client */
+        $result = $client->drushAliases();
+
+        $headers = $response->getHeader('Content-Type');
+        $this->assertEquals('application/gzip', reset($headers));
+
+        $this->assertNotInstanceOf('\ArrayObject', $result);
+        $this->assertInstanceOf('GuzzleHttp\Psr7\Stream', $result);
+    }
+}

af82b4c594faab892592cdb02a4ad2edb64e3a9c
Adam Malone <adam@adammalone.net>
2017-11-24 13:32:57 +1100

    Adds varnish purge test

---
+++ tests/Endpoints/VarnishTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class VarnishTest extends CloudApiTestCase
+{
+
+    public function testPurgeVarnish()
+    {
+        $response = $this->getPsr7JsonResponseForFixture('Endpoints/purgeVarnish.json');
+        $client = $this->getMockClient($response);
+
+        /** @var AcquiaCloudApi\CloudApi\Client $client */
+        $result = $client->purgeVarnishCache('14-0c7e79ab-1c4a-424e-8446-76ae8be7e851', ['example.com', 'www.example.com']);
+
+        $this->assertInstanceOf('\AcquiaCloudApi\Response\OperationResponse', $result);
+
+        $this->assertEquals('Varnish is being cleared for the selected domains.', $result->message);
+    }
+}

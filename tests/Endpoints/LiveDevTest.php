5b18da9b4b50a7c02f359d22c48d65133dd8b9f0
Adam Malone <adam@adammalone.net>
2017-11-24 13:30:35 +1100

    Adds livedev and production mode tests

---
+++ tests/Endpoints/LiveDevTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class LiveDevTest extends CloudApiTestCase
+{
+
+    public function testLiveDevEnable()
+    {
+        $response = $this->getPsr7JsonResponseForFixture('Endpoints/enableLiveDev.json');
+        $client = $this->getMockClient($response);
+
+        /** @var AcquiaCloudApi\CloudApi\Client $client */
+        $result = $client->enableLiveDev('14-0c7e79ab-1c4a-424e-8446-76ae8be7e851');
+
+        $this->assertInstanceOf('\AcquiaCloudApi\Response\OperationResponse', $result);
+
+        $this->assertEquals('Live Dev is being enabled.', $result->message);
+    }
+
+    public function testLiveDevDisable()
+    {
+        $response = $this->getPsr7JsonResponseForFixture('Endpoints/disableLiveDev.json');
+        $client = $this->getMockClient($response);
+
+        /** @var AcquiaCloudApi\CloudApi\Client $client */
+        $result = $client->disableLiveDev('14-0c7e79ab-1c4a-424e-8446-76ae8be7e851');
+
+        $this->assertInstanceOf('\AcquiaCloudApi\Response\OperationResponse', $result);
+
+        $this->assertEquals('Live Dev is being disabled.', $result->message);
+    }
+}
---
+++ tests/Endpoints/ProductionModeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+class ProductionModeTest extends CloudApiTestCase
+{
+
+    public function testProductionModeEnable()
+    {
+        $response = $this->getPsr7JsonResponseForFixture('Endpoints/enableProductionMode.json');
+        $client = $this->getMockClient($response);
+
+        /** @var AcquiaCloudApi\CloudApi\Client $client */
+        $result = $client->enableProductionMode('14-0c7e79ab-1c4a-424e-8446-76ae8be7e851');
+
+        $this->assertInstanceOf('\AcquiaCloudApi\Response\OperationResponse', $result);
+
+        $this->assertEquals('Production mode has been enabled for this environment.', $result->message);
+    }
+
+    public function testProductionModeDisable()
+    {
+        $response = $this->getPsr7JsonResponseForFixture('Endpoints/disableProductionMode.json');
+        $client = $this->getMockClient($response);
+
+        /** @var AcquiaCloudApi\CloudApi\Client $client */
+        $result = $client->disableProductionMode('14-0c7e79ab-1c4a-424e-8446-76ae8be7e851');
+
+        $this->assertInstanceOf('\AcquiaCloudApi\Response\OperationResponse', $result);
+
+        $this->assertEquals('Production mode has been disabled for this environment.', $result->message);
+    }
+}

9dabdf30b96cabb42e183f4652d5632c1d725670
Adam Malone <adam@adammalone.net>
2020-01-22 19:21:57 +1100

    Adds getMember and getAdmin calls to get single user records.

--- tests/Endpoints/MembersTest.php
+++ tests/Endpoints/MembersTest.php
@@ -40,6 +40,26 @@ class MembersTest extends CloudApiTestCase
         }
     }
 
+    public function testGetOrganizationMember()
+    {
+        $response = $this->getPsr7JsonResponseForFixture('Endpoints/Organizations/getMember.json');
+        $client = $this->getMockClient($response);
+
+        /** @var \AcquiaCloudApi\CloudApi\ClientInterface $client */
+        $organization = new Organizations($client);
+        $result = $organization->getMember(
+            '14-0c7e79ab-1c4a-424e-8446-76ae8be7e851',
+            'f2daa9cc-e5a0-4036-a5c8-f96e336c62b5'
+        );
+
+        $this->assertNotInstanceOf('\AcquiaCloudApi\Response\MembersResponse', $result);
+        $this->assertInstanceOf('\AcquiaCloudApi\Response\MemberResponse', $result);
+
+        foreach ($this->properties as $property) {
+            $this->assertObjectHasAttribute($property, $result);
+        }
+    }
+
     public function testGetOrganizationAdmins()
     {
         $response = $this->getPsr7JsonResponseForFixture('Endpoints/Organizations/getAdmins.json');
@@ -58,6 +78,26 @@ class MembersTest extends CloudApiTestCase
             foreach ($this->properties as $property) {
                 $this->assertObjectHasAttribute($property, $record);
             }
+        }
+    }
+
+    public function testGetOrganizationAdmin()
+    {
+        $response = $this->getPsr7JsonResponseForFixture('Endpoints/Organizations/getAdmin.json');
+        $client = $this->getMockClient($response);
+
+        /** @var \AcquiaCloudApi\CloudApi\ClientInterface $client */
+        $organization = new Organizations($client);
+        $result = $organization->getAdmin(
+            '14-0c7e79ab-1c4a-424e-8446-76ae8be7e851',
+            'f2daa9cc-e5a0-4036-a5c8-f96e336c62b5'
+        );
+
+        $this->assertNotInstanceOf('\AcquiaCloudApi\Response\MembersResponse', $result);
+        $this->assertInstanceOf('\AcquiaCloudApi\Response\MemberResponse', $result);
+
+        foreach ($this->properties as $property) {
+            $this->assertObjectHasAttribute($property, $result);
         }
     }
 

9ddac3b4dfb46c43b334d0f8ef9ce8f497972bc1
Adam Malone <adam@adammalone.net>
2020-01-23 18:16:02 +1100

    Updates InsightResponse with latest from API.

--- src/Response/InsightResponse.php
+++ src/Response/InsightResponse.php
@@ -9,9 +9,12 @@ namespace AcquiaCloudApi\Response;
 class InsightResponse
 {
 
+    public $uuid;
     public $label;
     public $hostname;
     public $status;
+    public $updatedAt;
+    public $lastConnectedAt;
     public $scores;
     public $counts;
     public $flags;
@@ -23,9 +26,12 @@ class InsightResponse
      */
     public function __construct($insight)
     {
+        $this->uuid = $insight->uuid;
         $this->label = $insight->label;
         $this->hostname = $insight->hostname;
         $this->status = $insight->status;
+        $this->updatedAt = $insight->updated_at;
+        $this->lastConnectedAt = $insight->last_connected_at;
         $this->scores = $insight->scores;
         $this->flags = $insight->flags;
         $this->links = $insight->_links;

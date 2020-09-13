d81ffa3352029e8c8857e1b8e1d69ac76cc55cf5
Erik Peterson <erik.peterson@acquia.com>
2020-09-13 23:19:05 +0000

    Tests for change-owner

--- tests/Endpoints/OrganizationsTest.php
+++ tests/Endpoints/OrganizationsTest.php
@@ -155,4 +155,20 @@ class OrganizationsTest extends CloudApiTestCase
             }
         }
     }
+
+    public function testChangeOwner()
+    {
+        $response = $this->getPsr7GzipResponseForFixture('Endpoints/Organizations/changeOwner.json');
+        $client = $this->getMockClient($response);
+
+        /** @var \AcquiaCloudApi\CloudApi\ClientInterface $client */
+        $organization = new Organizations($client);
+        $result  = $organization->changeOwner(
+            '14-0c7e79ab-1c4a-424e-8446-76ae8be7e851',
+            '82cff7ec-2f09-11e9-b210-d663bd873d93'
+        );
+
+        $this->assertInstanceOf('\AcquiaCloudApi\Response\OperationResponse', $result);
+        $this->assertEquals("Changed organization owner.", $result->message);
+    }
 }

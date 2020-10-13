cdc8cc624ff4404d729f9c09439f5d444e6eb420
Adam Malone <adam@adammalone.net>
2020-10-13 13:37:46 +1100

    Adds partial tests for createRequest method.

--- tests/Endpoints/ConnectorTest.php
+++ tests/Endpoints/ConnectorTest.php
@@ -6,6 +6,8 @@ use AcquiaCloudApi\Tests\CloudApiTestCase;
 use AcquiaCloudApi\Connector\Client;
 use AcquiaCloudApi\Connector\Connector;
 use AcquiaCloudApi\Endpoints\Applications;
+use League\OAuth2\Client\Test\Provider\Fake as MockProvider;
+use League\OAuth2\Client\Token\AccessToken;
 
 class ConnectorTest extends CloudApiTestCase
 {
@@ -27,5 +29,45 @@ class ConnectorTest extends CloudApiTestCase
             $connector::URL_ACCESS_TOKEN,
             'https://accounts.acquia.com/api/auth/oauth/token'
         );
+    }
+
+    public function testGetAuthenticatedRequest()
+    {
+        $config = [
+            'key' => 'key',
+            'secret' => 'secret'
+        ];
+
+        $connector = new Connector($config);
+        $reflectionClass = new \ReflectionClass('AcquiaCloudApi\Connector\Connector');
+
+        $token = new AccessToken(['access_token' => 'acquia-token', 'expires_in' => 300]);
+        $accessTokenProperty = $reflectionClass->getProperty('accessToken');
+        $accessTokenProperty->setAccessible(true);
+        $accessTokenProperty->setValue($connector, $token);
+
+        $provider = new MockProvider();
+        $providerProperty = $reflectionClass->getProperty('provider');
+        $providerProperty->setAccessible(true);
+        $providerProperty->setValue($connector, $provider);
+
+        $request = $connector->createRequest('get', '/account');
+        $this->assertInstanceOf('GuzzleHttp\Psr7\Request', $request);
+
+        $expectedHeaders = [
+            'Host' => [
+                'cloud.acquia.com',
+            ],
+            'Authorization' => [
+                'Bearer acquia-token',
+            ]
+        ];
+        $expectedHeaderNames = [
+            'authorization' => 'Authorization',
+            'host' => 'Host',
+        ];
+
+        $this->assertAttributeSame($expectedHeaders, 'headers', $request);
+        $this->assertAttributeSame($expectedHeaderNames, 'headerNames', $request);
     }
 }

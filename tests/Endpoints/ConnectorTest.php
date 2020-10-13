3f59598126d6fd8138a126be23e152a2edabe381
Adam Malone <adam@adammalone.net>
2020-10-13 16:32:43 +1100

    Test the other half of createRequest.

--- tests/Endpoints/ConnectorTest.php
+++ tests/Endpoints/ConnectorTest.php
@@ -14,8 +14,7 @@ use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\Request;
-
-// use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class ConnectorTest extends CloudApiTestCase
 {
@@ -174,24 +173,49 @@ class ConnectorTest extends CloudApiTestCase
 
     public function testGetAuthenticatedRequest()
     {
+        // Clear the cache to make sure we get fresh results during testing.
+        $cache = new FilesystemAdapter();
+        $foo = $cache->deleteItem('cloudapi-token');
+
         $config = [
             'key' => 'key',
             'secret' => 'secret'
         ];
 
+        // Create a new Connector and override the provider property set in the constructor.
         $connector = new Connector($config);
         $reflectionClass = new \ReflectionClass('AcquiaCloudApi\Connector\Connector');
 
-        $token = new AccessToken(['access_token' => 'acquia-token', 'expires_in' => 300]);
-        $accessTokenProperty = $reflectionClass->getProperty('accessToken');
-        $accessTokenProperty->setAccessible(true);
-        $accessTokenProperty->setValue($connector, $token);
+        $provider = new MockProvider([
+            'clientId' => 'mock_client_id',
+            'clientSecret' => 'mock_secret',
+            'redirectUri' => 'none',
+        ]);
 
-        $provider = new MockProvider();
         $providerProperty = $reflectionClass->getProperty('provider');
         $providerProperty->setAccessible(true);
         $providerProperty->setValue($connector, $provider);
 
+        // Create the mock response from the call to get the access token.
+        $expires = time() + 300;
+        $raw_response = ['access_token' => 'acquia-token', 'expires' => $expires, 'resource_owner_id' => 3];
+
+        $grant = Phony::mock(AbstractGrant::class);
+        $grant->prepareRequestParameters->returns([]);
+
+        $stream = Phony::mock(StreamInterface::class);
+        $stream->__toString->returns(json_encode($raw_response));
+
+        $response = Phony::mock(ResponseInterface::class);
+        $response->getBody->returns($stream->get());
+        $response->getHeader->with('content-type')->returns('application/json');
+
+        $client = Phony::mock(ClientInterface::class);
+        $client->send->returns($response->get());
+
+        $provider->setHttpClient($client->get());
+
+        // Create the request and check it matches our expectations.
         $request = $connector->createRequest('get', '/account');
         $this->assertInstanceOf('GuzzleHttp\Psr7\Request', $request);
 
@@ -210,5 +234,19 @@ class ConnectorTest extends CloudApiTestCase
 
         $this->assertAttributeSame($expectedHeaders, 'headers', $request);
         $this->assertAttributeSame($expectedHeaderNames, 'headerNames', $request);
+
+        // Check the cache to make sure that the token has been cached successfully.
+        $cache = new FilesystemAdapter();
+        $accessToken = $cache->getItem('cloudapi-token')->get();
+
+        // Ensure that the cached item is an AccessToken and that it contains the values we set above.
+        $this->assertInstanceOf('League\OAuth2\Client\Token\AccessToken', $accessToken);
+        $this->assertAttributeSame('acquia-token', 'accessToken', $accessToken);
+        $this->assertAttributeSame(3, 'resourceOwnerId', $accessToken);
+        $this->assertAttributeSame($expires, 'expires', $accessToken);
+
+        // Delete the cached token again to clean up.
+        $delete = $cache->deleteItem('cloudapi-token');
+        $this->assertTrue($delete);
     }
 }

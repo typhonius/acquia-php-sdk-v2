09718fd9f9968da98a8ac6734bca9fd4ea02cc4a
Dane Powell <git@danepowell.com>
2023-03-09 10:32:11 -0800

    Fixup getEnvironmentsDatabases

--- src/Response/DatabaseResponse.php
+++ src/Response/DatabaseResponse.php
@@ -18,5 +18,10 @@ class DatabaseResponse
         $this->name = $database->name;
         $this->user_name = $database->user_name;
         $this->password = $database->password;
+        $this->url = $database->url;
+        $this->db_host = $database->db_host;
+        $this->ssh_host = $database->ssh_host;
+        $this->flags = $database->flags;
+        $this->environment = $database->environment;
     }
 }

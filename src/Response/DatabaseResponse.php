642b23ec5815e0b088d25ad077ed404ac2fbd276
Dane Powell <git@danepowell.com>
2023-03-09 10:15:49 -0800

    Add full database responses (#348)

--- src/Response/DatabaseResponse.php
+++ src/Response/DatabaseResponse.php
@@ -5,9 +5,18 @@ namespace AcquiaCloudApi\Response;
 class DatabaseResponse
 {
     public string $name;
+    public string $user_name;
+    public string $password;
+    public string $url;
+    public string $db_host;
+    public string $ssh_host;
+    public object $flags;
+    public object $environment;
 
     public function __construct(object $database)
     {
         $this->name = $database->name;
+        $this->user_name = $database->user_name;
+        $this->password = $database->password;
     }
 }

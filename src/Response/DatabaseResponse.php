1a43bafbae3978cf8aac51f6f6cd0ac509885adb
Dane Powell <git@danepowell.com>
2023-05-03 12:17:56 -0700

    CLI-1046: user_name is null (#384)
    * CLI-1046: user_name is null
    
    * use coalescing operator
    
    * Add db id

--- src/Response/DatabaseResponse.php
+++ src/Response/DatabaseResponse.php
@@ -4,23 +4,27 @@ namespace AcquiaCloudApi\Response;
 
 class DatabaseResponse
 {
+    public string $id;
     public string $name;
-    public string $user_name;
-    public string $password;
-    public string $url;
+    // Connection details will be missing without the required permission:
+    // "View database connection details (username, password, or hostname)"
+    public ?string $user_name;
+    public ?string $password;
+    public ?string $url;
     public string $db_host;
-    public string $ssh_host;
+    public ?string $ssh_host;
     public object $flags;
     public object $environment;
 
     public function __construct(object $database)
     {
+        $this->id = $database->id;
         $this->name = $database->name;
-        $this->user_name = $database->user_name;
-        $this->password = $database->password;
-        $this->url = $database->url;
+        $this->user_name = $database->user_name ?? null;
+        $this->password = $database->password ?? null;
+        $this->url = $database->url ?? null;
         $this->db_host = $database->db_host;
-        $this->ssh_host = $database->ssh_host;
+        $this->ssh_host = $database->ssh_host ?? null;
         $this->flags = $database->flags;
         $this->environment = $database->environment;
     }
